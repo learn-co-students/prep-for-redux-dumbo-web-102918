e7f9f8900750e9bd5f5ea84095eaa71cac6798e0
oazzat <oazzat19@gmail.com>
2019-01-25 11:06:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -5,22 +5,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+state = {
+  count: 0
+}
+
+updateCount = (count) =>{
+  this.setState({count: count})
+}
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Counter />
+        <Header count={this.state.count}/>
+        <Counter updateCount={this.updateCount}/>
       </div>
     );
   }
 }
 
 class Header extends Component {
+
+
+  renderDescription = () => {
+    const remainder = this.props.count % 5;
+    const upToNext = 5 - remainder;
+    return `The current count is less than ${this.props.count + upToNext}`;
+  };
+
+
+
   render() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
+        <h3>{this.renderDescription()}</h3>
       </header>
     );
   }
@@ -30,18 +49,15 @@ class Counter extends Component {
   state = { count: 0 };
 
   increment = () => {
-    this.setState(prevState => ({ count: prevState.count + 1 }));
+    this.setState(prevState => ({ count: prevState.count + 1 }),()=>this.props.updateCount(this.state.count));
+  
   };
 
   decrement = () => {
-    this.setState(prevState => ({ count: prevState.count - 1 }));
+    this.setState(prevState => ({ count: prevState.count - 1 }),()=> this.props.updateCount(this.state.count));
+
   };
 
-  renderDescription = () => {
-    const remainder = this.state.count % 5;
-    const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
-  };
 
   render() {
     return (
@@ -49,7 +65,7 @@ class Counter extends Component {
         <h1>{this.state.count}</h1>
         <button onClick={this.decrement}> - </button>
         <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+
       </div>
     );
   }
